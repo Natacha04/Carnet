a692e8f5ff65d539341a9660b2d7a92fb08b6bd6
Natacha04 <drostnatacha@gmail.com>
2026-02-11 15:04:54 +0100

    cool

--- src/components/AddContact.tsx
+++ src/components/AddContact.tsx
@@ -1,56 +1,31 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { useState } from "react";
-import { Alert, Button, StyleSheet } from "react-native";
-import { Text } from "react-native-gesture-handler"
+/* eslint-disable react-native/no-inline-styles */
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-gesture-handler';
 
-type propsContact= {
-    nom : string;
-    telephone : number;
-}
+type propsContact = {
+  nom: string;
+  telephone: number;
+};
 
-function AddContact( props: propsContact ) {
-     const [nom, setNom] = useState('');
-
-     function handleData(text: string) {
-    setNom(text);
-
-  async function add() {
-    try {
-      await AsyncStorage.setItem('name', name);
-      Alert.alert('Le nom a été ajouté avec succès!')
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-       const handlePress = () => {
-    Alert.alert('Informations enregistrées !');
-  };
-
-    const Tab = createMaterialTopTabNavigator();
-
-  
-  }
-    return(
-        <>
-        <Text style={{ color: 'black', fontSize: 18, fontWeight: '600', padding: 10 }} >Nom : {props.nom}</Text>
-         <Text style={styles.text}>Telephone : {props.telephone}</Text>
-          <TextInput placeholder='Saisir votre nom' onChangeText={handleData}></TextInput>
-          <Text>Nom : {nom}</Text>
-          <TextInput placeholder='Saisir votre numéro de téléphone' onChangeText={handleData}></TextInput>
-          <Text>Téléphone : {telephone}</Text>
-           <Button title="Ajouter" onPress={add} />
-        </>
-    );
+function AddContact(props: propsContact) {
+  return (
+    <>
+      <Text
+        style={{ color: 'black', fontSize: 18, fontWeight: '600', padding: 10 }}
+      >
+        Nom : {props.nom}
+      </Text>
+      <Text style={styles.text}>Telephone : {props.telephone}</Text>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-   text: {
+  text: {
     color: 'black',
-     fontSize: 18,
-      padding: 10,
-  }
+    fontSize: 18,
+    padding: 10,
+  },
 });
 
 export default AddContact;
